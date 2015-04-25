0f8c4a54ee2e9e32f043ed61104d13ce209c87d9
roccons <contacto@habitatweb.com.mx>
2015-04-24 20:15:29 -0500

    crédito

--- es6.js
+++ es6.js
@@ -1,5 +1,9 @@
-/* Aquí va el código en ES6... ¡a jugar! */
+/* 
+Aquí va el código en ES6... ¡a jugar!
+Idea basada en https://github.com/sergiodxa/es6
+*/
+
 
 let nombre = `Don Pepito
-y Donn José`;
+y Don José`;
 console.log(`Hola ${nombre}`);
