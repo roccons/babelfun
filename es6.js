4d355f8205452154137072f3d267e972f9eaf2d8
roccons <contacto@habitatweb.com.mx>
2015-04-24 20:33:12 -0500

    texto para index.html

--- es6.js
+++ es6.js
@@ -3,7 +3,9 @@ Aquí va el código en ES6... ¡a jugar!
 Idea basada en https://github.com/sergiodxa/es6
 */
 
-
+let mitexto = document.querySelector('.mitexto');
 let nombre = `Don Pepito
 y Don José`;
+
 console.log(`Hola ${nombre}`);
+mitexto.innerHTML = `Hola ${nombre}`;
